20f27f6f7d6779ee482dda86a2336bac869ba79b
Joseph Stewart <josephlikescode@gmail.com>
2024-12-14 14:28:19 -0800

    Fix tsconfig and eslint.

--- packages/fsd-gen/.eslintrc.cjs
+++ packages/fsd-gen/.eslintrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  ignorePatterns: [".eslintrc.cjs"],
+  ignorePatterns: [".eslintrc.cjs", "dist/"],
   extends: ["@noflake/eslint-config/index.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
